869628729e69f04f935e8f01deb08cbff77b97fa
Tobias Steiner <tobias@MacBook-Air-von-Tobias.local>
2025-09-29 01:14:13 +0200

    Configure Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/root/',
-  build: { outDir: 'docs/root' }
+  base: '/',
+  build: { outDir: 'dist' }
 })
