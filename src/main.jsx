8e08080fda9e141d582a624067e4b6df4f395919
Tobias Steiner <tobias@MacBook-Air-von-Tobias.local>
2025-09-30 15:54:55 +0200

    Use HashRouter in production to fix deep links on Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,16 @@
 // src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import App from "./App";
 import "/src/styles.css";
 
-// Polyfills (falls nötig)
-import { Buffer } from "buffer";
-if (!window.Buffer) window.Buffer = Buffer;
-if (!window.process) window.process = { env: {} };
+const Router = import.meta.env.PROD ? HashRouter : BrowserRouter;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* nutzt in dev '/' und im Build den Wert aus vite.config.js */}
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
